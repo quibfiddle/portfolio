92cb1d8d34033342e2438ec779554f618bf22798
Stuart <bingham.stuart@gmail.com>
2018-01-20 05:21:11 +0000

    adds flash messages for message feedback

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var methodOverride = require("method-override");
-var nodemailer = require("nodemailer");
-var bodyParser = require("body-parser");
-var expressSanitizer = require("express-sanitizer");
+var express             = require("express");
+var methodOverride      = require("method-override");
+var nodemailer          = require("nodemailer");
+var bodyParser          = require("body-parser");
+var expressSanitizer    = require("express-sanitizer");
+var flash               = require("connect-flash");
 
 var app = express();
 
@@ -12,6 +13,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
+app.use(flash());
+
+app.use(require("express-session")({
+    secret: "Nicolas Cage is the best",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(function(req, res, next){
+    res.locals.error = req.flash("error")
+    res.locals.success = req.flash("success");
+   next();
+});
+
     
 app.get("/", function(req, res){
       res.render("stuart")
@@ -48,12 +63,14 @@ app.post("/contact", function(req, res){
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
+            req.flash("error", "Looks like that message did not go through for some reason, Feel free to email me directly at bingham.stuart@gmail.com with your questions")
             return console.log(error);
         }
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
+        req.flash("success", "Thanks for reaching out! I'll get back to you at your provided email address as soon as I can.");
         res.redirect("/");
     });
 });
