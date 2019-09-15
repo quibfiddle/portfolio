24a69a0e53e3a3fd51a06183f5919249f0bc16fb
Ubuntu <ubuntu@ip-172-31-44-162.us-east-2.compute.internal>
2019-09-15 22:56:26 +0000

    adds letsencrypt routes

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.get("/resume", function(req, res){
       res.render("resume")
 });
 
+app.get("/.well-known/acme-challenge/5KBBsqOZDE0mVfMYWzgI59PY9pQ08z7cidOX5Z_w8wU", function(req, res){
+      res.render("letsencrypt")
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server has started");
